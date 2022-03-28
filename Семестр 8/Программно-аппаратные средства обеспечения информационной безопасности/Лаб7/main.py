b44a6e10e50b67720730dc51b4942c9d3b3f6651
Alex Ivlev <alexfromsuvorov@gmail.com>
2022-03-28 10:51:52 +0300

    28.03.2022

--- Семестр 8/Программно-аппаратные средства обеспечения информационной безопасности/Лаб7/main.py
+++ Семестр 8/Программно-аппаратные средства обеспечения информационной безопасности/Лаб7/main.py
@@ -1,15 +1,15 @@
+from DESForm import Ui_DESForm
+from KeyModeForm import Ui_KeyModeForm
+from PyQt5.QtWidgets import QDialog, QMainWindow, QTableWidgetItem, QFileDialog, QApplication
+from qt_material import apply_stylesheet
+import sys
+from datetime import datetime
 from Crypto.Cipher import DES
-from tkinter import *
-from tkinter import ttk
-from tkinter import filedialog
-import ctypes
+from crc64iso.crc64iso import crc64
 
 
 class DESEncoder:
-    mode = None
-
-    def __init__(self):
-        self.mode = DES.MODE_ECB
+    mode = DES.MODE_ECB
 
     @staticmethod
     def __append_last_bytes(text):
@@ -20,93 +20,85 @@ class DESEncoder:
     @staticmethod
     def __delete_last_bytes(text):
         length = text[-1]
+        if length > 8:
+            raise Exception()
         return text[:-length]
 
     def encrypt_des(self, text, key):
         text = self.__append_last_bytes(text)
-        des = DES.new(key, self.mode)
+        des = DES.new(bytes.fromhex(crc64(key)), self.mode)
         return des.encrypt(text)
 
     def decrypt_des(self, text, key):
-        des = DES.new(key, self.mode)
+        des = DES.new(bytes.fromhex(crc64(key)), self.mode)
         text = des.decrypt(text)
         return self.__delete_last_bytes(text)
 
 
-class DESForm(Tk):
-    des = None
-    key = None
+class KeyModeForm(QDialog):
 
-    class KeyModeForm(Toplevel):
-        key = None
-        mode = None
+    def __init__(self, master):
+        super().__init__()
+        self.ui = Ui_KeyModeForm()
+        self.ui.setupUi(self)
+        self.ui.btnOK.clicked.connect(self.accept)
+        self.master = master
 
-        def __init__(self, master=None):
-            super().__init__(master=master)
+    def accept(self):
+        mode = [DES.MODE_ECB, DES.MODE_CBC, DES.MODE_CFB, DES.MODE_OFB]
+        self.master.des.mode = mode[self.ui.cbMode.currentIndex()]
+        self.master.key = self.ui.leKey.text()
+        super().accept()
 
-            self.title("Enter Key and Mode")
-            self.geometry("220x100")
-            frm = ttk.Frame(self, padding=10)
-            frm.grid()
-            ttk.Label(frm, text="Key", width=10).grid(column=0, row=0)
-            self.key = ttk.Entry(frm, show="*", width=20)
-            self.key.grid(column=1, row=0)
-            ttk.Label(frm, text="Mode", width=10).grid(column=0, row=1)
-            self.mode = ttk.Combobox(frm, values=["ECB", "CBC", "CFB", "OFB"], width=17)
-            self.mode.current(0)
-            self.mode.grid(column=1, row=1)
-            ttk.Button(frm, text="Apply", width=20, command=self.destroy).grid(column=1, row=2)
 
-        def destroy(self):
-            mode = {"ECB": DES.MODE_ECB, "CBC": DES.MODE_CBC, "CFB": DES.MODE_CFB, "OFB": DES.MODE_OFB}
-            self.master.des.mode = mode[self.mode.get()]
-            self.master.key = self.key.get()
-            super().destroy()
+class DESForm(QMainWindow):
 
-    def __init__(self, screenName=None, baseName=None, className='DESForm',
-                 useTk=True, sync=False, use=None):
-        super().__init__(screenName, baseName, className, useTk, sync, use)
-        self.title("DES Encoder")
-        frm = ttk.Frame(self, padding=10)
-        frm.grid()
-        ttk.Button(frm, text="Encrypt File...", width=50, command=self.encrypt).grid(column=0, row=0)
-        ttk.Button(frm, text="Decrypt File...", width=50, command=self.decrypt).grid(column=0, row=1)
-
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_DESForm()
+        self.ui.setupUi(self)
+        self.ui.btnEncrypt.clicked.connect(lambda: self.encrypt_decrypt(encrypt=True))
+        self.ui.btnDecrypt.clicked.connect(lambda: self.encrypt_decrypt(encrypt=False))
         self.des = DESEncoder()
 
-    @staticmethod
-    def __message(title, text, style):
-        return ctypes.windll.user32.MessageBoxW(0, text, title, style)
+    def __add_result(self, result):
+        row = self.ui.tblResult.rowCount()
+        self.ui.tblResult.setRowCount(row + 1)
+        for i in range(len(result)):
+            self.ui.tblResult.setItem(row, i, QTableWidgetItem(str(result[i])))
 
-    def encrypt(self):
+    def encrypt_decrypt(self, encrypt=True):
+        file_path = ''
         try:
-            file_path = filedialog.askopenfilename()
+            file_path = \
+                QFileDialog.getOpenFileName(self, 'Open file', filter="" if encrypt else "Encrypted files (*.enc)")[0]
             with open(file_path, 'rb') as file:
                 text = file.read()
-            key_form = self.KeyModeForm(master=self)
-            while key_form is not None:
-                self.update()
-            text = self.des.encrypt_des(text, self.key)
-            with open(file_path + '.enc', 'wb') as file:
+            key_form = KeyModeForm(self)
+            if not key_form.exec():
+                raise Exception()
+            text = self.des.encrypt_des(text, self.key) if encrypt else self.des.decrypt_des(text, self.key)
+            if not len(text):
+                raise Exception()
+            with open(file_path + '.enc' if encrypt else file_path[:-4], 'wb') as file:
                 file.write(text)
-            self.__message('Complete', 'File has been encrypted', 64)
+            result = 'OK'
         except Exception as e:
-            self.__message('Error', 'File cannot be encrypted', 16)
-
-    def decrypt(self):
-        try:
-            file_path = filedialog.askopenfilename(filetypes=[("Encrypted files", "*.enc")])
-            with open(file_path, 'rb') as file:
-                text = file.read()
-            self.KeyModeForm(master=self)
-            self.wait_window(self.KeyModeForm)
-            text = self.des.decrypt_des(text, self.key)
-            with open(file_path[:-4], 'wb') as file:
-                file.write(text)
-            self.__message('Complete', 'File has been decrypted', 64)
-        except:
-            self.__message('Error', 'File cannot be decrypted', 16)
+            result = 'Failure'
+            print(e)
+        if file_path != '':
+            self.__add_result([file_path, 'Encryption' if encrypt else 'Decryption', result,
+                               datetime.now().strftime("%b %d %Y %H:%M:%S")])
 
 
-des = DESForm()
-des.mainloop()
+class DESApp(QApplication):
+
+    def __init__(self):
+        super().__init__(sys.argv)
+        self.window = DESForm()
+        self.window.show()
+        apply_stylesheet(self, theme='dark_blue.xml')
+
+
+App = DESApp()
+sys.exit(App.exec())
